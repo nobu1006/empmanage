f2d3493e3eac0b05276366dd5eccc1007dd789c8
Nobuhiko Tobita <nobu1006@gmail.com>
2025-10-27 16:43:26 +0900

    登録処理も実装

--- src/main/java/com/example/empmanage/controller/EmployeeController.java
+++ src/main/java/com/example/empmanage/controller/EmployeeController.java
@@ -4,12 +4,15 @@ import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.Map;
 
+import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.example.empmanage.domain.Department;
+import com.example.empmanage.domain.Employee;
+import com.example.empmanage.form.EmployeeForm;
 import com.example.empmanage.service.DepartmentService;
 import com.example.empmanage.service.EmployeeService;
 
@@ -41,6 +44,21 @@ public class EmployeeController {
         return "employee/addForm";
     }
 
+    @RequestMapping("/add")
+    public String add(EmployeeForm form) {
+        Employee employee = new Employee();
+        BeanUtils.copyProperties(form, employee);
+        employeeService.save(employee);
+        return "redirect:/employee/list";
+    }
+    /**
+     * リストのまま画面にもっていってもおそらくプルダウンメニューは作れるが
+     * 一応習った通りマップにして、マップをth:eachで回す構造にしたいので
+     * リストからマップへの変換処理作成
+     * 
+     * @param depList
+     * @return
+     */
     private Map<Integer, String> convertList2Map(List<Department> depList) {
         Map<Integer, String> departmentMap = new LinkedHashMap<>();
         for (Department dep : depList) {
---
+++ src/main/java/com/example/empmanage/form/EmployeeForm.java
@@ -0,0 +1,52 @@
+package com.example.empmanage.form;
+
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.NotNull;
+
+public class EmployeeForm {
+
+    private Integer id;
+    @NotBlank(message="名前を入力してください")
+    private String name;
+    @NotNull(message="年齢を入力してください")
+    private Integer age;
+    @NotEmpty(message="性別を選択してください")
+    private String gender;
+    @NotEmpty(message="部署を選択してください")
+    private Integer departmentId;
+
+    public Integer getId() {
+        return id;
+    }
+    public void setId(Integer id) {
+        this.id = id;
+    }
+    public String getName() {
+        return name;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+    public Integer getAge() {
+        return age;
+    }
+    public void setAge(Integer age) {
+        this.age = age;
+    }
+    public String getGender() {
+        return gender;
+    }
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+    public Integer getDepartmentId() {
+        return departmentId;
+    }
+    public void setDepartmentId(Integer departmentId) {
+        this.departmentId = departmentId;
+    }
+
+    
+
+}
